25932f046d82be96dd01e5de8b60551be1318b78
litu <root@ip-172-31-33-223.ap-south-1.compute.internal>
2023-02-25 17:31:56 +0000

    7th commit

--- file1.java
+++ file1.java
@@ -1,8 +1,2 @@
-e hasrat hai mujhuko mjhe hos nehni ae .
-teri chat ke diwane hua hum 
-dil jigar najar kiya hai main to tere liya jan vi dedu.
-<<<<<<< HEAD
-hum unse mahobat karke din rat sanm rote hain
-=======
-tere sine main hum dil banke sanam bujh te hain or jal tehain
->>>>>>> issue2
+diwana dil hua diwana:x
+
